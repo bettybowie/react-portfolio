e0371dd46d5e280a6a8cb84d480bcdab5438b680
Betty Lu <bettylu@Bettys-MacBook-Pro.local>
2023-04-08 22:00:17 -0700

    project setup

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import { useState } from 'react';
+import AboutMe from './components/Tabs/AboutMe';
+import Contact from './components/Tabs/Contact';
+import NavTabs from './components/NavTabs';
+import Portfolio from './components/Tabs/Portfolio';
+import Resume from './components/Tabs/Resume';
+
+function App() {
+  const [currentPage, setCurrentPage] = useState('AboutMe');
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if (currentPage === 'AboutMe') {
+      return <AboutMe />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    return <Resume />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      {/* We are passing the currentPage from state and the function to update it */}
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* Here we are calling the renderPage method which will return a component  */}
+      {renderPage()}
+    </div>
+  );
+}
+
+export default App;
